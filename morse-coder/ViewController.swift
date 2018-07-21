6b478eafc4d4eb7c15de497c8032b8553a95e5d3
macos <macos@macoss-Mac.local>
2018-07-21 01:47:52 -0700

    initialized XCode project

---
+++ morse-coder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  morse-coder
+//
+//  Created by macos on 7/21/18.
+//  Copyright © 2018 mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
